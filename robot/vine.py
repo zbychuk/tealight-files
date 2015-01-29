3d23dcea0e94a8de78abe60d962260ed9310884d
zbychuk <zbych@controtex.com>
2015-01-29 23:37:19 +0000

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -9,7 +9,20 @@ from tealight.robot import (move,
 # Add your code here
 
 def go():
+  steps = 0
   while touch() == "fruit":
+    move()
+    steps += 1
+  if left_size() == "fruit":
+    turn(-1)
+    go()
+    turn(-1)
+  if right_size() == "fruit":
+    turn(1)
+    go()
+    turn(1)
+  turn(2)
+  for i in range(steps):
     move()
     
 go()
